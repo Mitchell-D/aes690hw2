f5adae160b8baccd6fd607456d46ca64334e0777
Mitchell-D <mtd0012@uah.edu>
2024-02-07 22:27:06 -0600

    Wrote data compression script

--- test_s3fs.py
+++ test_s3fs.py
@@ -11,6 +11,7 @@ from pathlib import Path
 def get_gmgsi(dataset, init_time:datetime, num_hours:int):
     """
     Query the GMGSI bucket for a continuous time range of hourly data
+
     :@param dataset: GMGSI dataset key; one of {"lw", "ssr", "sw", "vis", "wv"}
     :@param init_time: datetime object for the first hour of the desired range
     :@param num_hours: Integer number of hours from init_time for which data
@@ -25,9 +26,9 @@ def get_gmgsi(dataset, init_time:datetime, num_hours:int):
     bucket_path = f"s3://noaa-gmgsi-pds/GMGSI_{dataset.upper()}"
     ## Enumerate bucket paths for all valid hourly timesteps
     timesteps = [init_time+i*timedelta(hours=1) for i in range(num_hours)]
-    bpath = lambda t:bucket_path+t.strftime("/%Y/%m/%d/%H/*")
     ## Query the s3 bucket for all of the files
     bucket = s3fs.S3FileSystem(anon=True)
+    bpath = lambda t:bucket_path+t.strftime("/%Y/%m/%d/%H/*")
     paths = sorted(list(chain(*[bucket.glob(bpath(t)) for t in timesteps])))
     files = list(map(bucket.open, paths))
     ## Open and aggregate the files along the time axis as an xarray Dataset
@@ -35,12 +36,14 @@ def get_gmgsi(dataset, init_time:datetime, num_hours:int):
 
 if __name__=="__main__":
     """ Load imagery for the full day of 20240115 """
-    ds = get_gmgsi(dataset="lw", init_time=datetime(2024, 1, 15), num_hours=24)
+    ds = get_gmgsi(dataset="lw", init_time=datetime(2024, 1, 15), num_hours=2)
     #np.save("tmp.npy", np.array(ds["data"]))
 
-    """ Extract and averagevalues over SEUS """
-    geo_mask = (ds["lat"][...]>=25) & (ds["lat"][...]<35) & \
-            (ds["lon"]>-75)[...] & (ds["lon"][...]>-95)
+    """ Extract and average values over SEUS """
+    lat0,latf = (25,35)
+    lon0,lonf = (-75,-95)
+    geo_mask = (ds["lat"][...]>=lat0) & (ds["lat"][...]<latf) & \
+            (ds["lon"]>-lon0)[...] & (ds["lon"][...]>-lonf)
     geo_mask = np.broadcast_to(geo_mask, ds["data"].shape)
-    ds = np.array(ds["data"])[geo_mask]
-    print(f"Average over SEUS: {np.average(ds)}")
+    ds = ds["data"].where(geo_mask)
+    print(f"Average over SEUS: {np.nanmean(ds)}")
